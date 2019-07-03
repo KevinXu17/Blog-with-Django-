f6321df0911ec9c5a749ea39a8fe733f0a2b6d25
Kevin Xu <kevinxu@KevindeMacBook-Pro.local>
2019-07-03 15:48:18 -0700

    add post form of product that create new product in database

--- venv/src/course/products/views.py
+++ venv/src/course/products/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ProductForm
 from .models import Product
+
 # Create your views here.
-def product_detail_view(request):
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
     # query
-    obj = Product.objects.get(id = 3)
+    # obj = Product.objects.get(id = 3)
     # context = {
     #     'title': obj.title,
     #     'description': obj.description
     # }
+    return render(request, "products/product_create.html", context)
+
+def product_detail_view(request):
+    objs = Product.objects.all()
     context = {
-        'obj': obj
+        'objs': objs
     }
-    return render(request, "products/product_detail.html", context)
+    return render(request, 'products/product_detail.html', context)
