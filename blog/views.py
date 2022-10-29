f649dbe0f3d154d2ef1c1f0f564f8a395f924a22
KevinXu17 <xuzhaozhishu@gmail.com>
2022-10-28 19:57:30 -0700

    # What have Done# get rid of dummy data, use default database for blogs

--- blog/views.py
+++ blog/views.py
@@ -3,27 +3,10 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
-## dummy dataset
-posts = [
-    {
-        'author': 'test1',
-        'title': 'test Blog',
-        'content': 'test 1 with dummy data',
-        'date_posted': 'August 23, 2022'
-    },
-    {
-        'author': 'test2',
-        'title': 'test Blog 2',
-        'content': 'test 2 with dummy data',
-        'date_posted': 'August 24, 2022'
-    },
-]
-
-
 def home(request):
     context = {
         ## Post.objects.all()
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
