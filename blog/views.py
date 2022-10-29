75e9cccc4024d6b26dab19352ccf8c18451cb218
KevinXu17 <xuzhaozhishu@gmail.com>
2022-10-28 19:38:42 -0700

    # What have Done# 1)migrate blog model to database 2)set up dummy blog data for tesing with html file

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
+## dummy dataset
+posts = [
+    {
+        'author': 'test1',
+        'title': 'test Blog',
+        'content': 'test 1 with dummy data',
+        'date_posted': 'August 23, 2022'
+    },
+    {
+        'author': 'test2',
+        'title': 'test Blog 2',
+        'content': 'test 2 with dummy data',
+        'date_posted': 'August 24, 2022'
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        ## Post.objects.all()
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
