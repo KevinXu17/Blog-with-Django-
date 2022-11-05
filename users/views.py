265a1d9915019ac4d6968972ba8d098294aaacfa
KevinXu17 <xuzhaozhishu@gmail.com>
2022-11-05 10:21:07 -0700

    ## What have done ## 1) add login logout views 2) login form using Django user form 3) setup login/logout/register link to nav bar and btn

--- users/views.py
+++ users/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .form import UserRegisterForm
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print("Post in")
+        # print("Post in")
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account has been created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
