1d7083aa1f51661feb5fc0fd6303e7346cf7cf76
KevinXu17 <xuzhaozhishu@gmail.com>
2022-11-04 22:28:08 -0700

    ## What have done ## 1)use gspread do CRUD 2)update readme with database using gspread

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Django
+
+## 1 database
+use google spreadsheets as database
+1) create project in google cloud
+2) add google drive & google spreadsheet API
+3) create credentials key
+4) shared google spreadsheets with that cred key email
+5) APP install [gspread library](https://github.com/burnash/gspread)
--- spreadsheet.py
+++ spreadsheet.py
@@ -8,6 +8,10 @@ gc = gspread.service_account(filename='secret.json')
 sheet = gc.open("Legislators").sheet1
 
 # records = sheet.get_all_records()
-record = sheet.row_values(6)
+# update_cell(x, y , value)
+# insert_row(values, x)
+# delete_row
+row_record = sheet.row_values(6)
+col_record = sheet.col_values(3)
 pp = pprint.PrettyPrinter()
-pp.pprint(record)
+pp.pprint(col_record)
